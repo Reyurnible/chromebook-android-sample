b04c284ba1d8da2c5aa3e7bb8d4a1d4b49455a16
ShunHosaka <reyurnible@gmail.com>
2019-01-15 01:42:15 +0900

    [add] add cart button for product items

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/component/products/staggeredgridlayout/StaggeredProductCardRecyclerViewAdapter.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/component/products/staggeredgridlayout/StaggeredProductCardRecyclerViewAdapter.kt
@@ -9,7 +9,10 @@ import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
  * Adapter used to show an asymmetric grid of products, with 2 items in the first column, and 1
  * item in the second column, and so on.
  */
-class StaggeredProductCardRecyclerViewAdapter(private val productList: List<ProductEntry>?) : RecyclerView.Adapter<StaggeredProductCardViewHolder>() {
+class StaggeredProductCardRecyclerViewAdapter(
+    var productList: List<ProductEntry> = emptyList(),
+    var listener: StaggeredProductCardRecyclerViewAdapterListener? = null
+) : RecyclerView.Adapter<StaggeredProductCardViewHolder>() {
 
     override fun getItemViewType(position: Int): Int = position % 3
 
@@ -20,14 +23,22 @@ class StaggeredProductCardRecyclerViewAdapter(private val productList: List<Prod
             1 -> StaggeredProductCardViewHolder.Second(inflater, parent)
             2 -> StaggeredProductCardViewHolder.Third(inflater, parent)
             else -> throw IllegalArgumentException("view type is require 0 to 2")
+        }.apply {
+            addCartButtonClickListener = { _, product ->
+                listener?.onProductAddCartClicked(product)
+            }
         }
     }
 
     override fun onBindViewHolder(holder: StaggeredProductCardViewHolder, position: Int) {
-        productList?.getOrNull(position)?.let {
-            holder.bind(it)
+        productList.getOrNull(position)?.let {
+            holder.product = it
         }
     }
 
-    override fun getItemCount(): Int = productList?.size ?: 0
+    override fun getItemCount(): Int = productList.size ?: 0
+
+    interface StaggeredProductCardRecyclerViewAdapterListener {
+        fun onProductAddCartClicked(product: ProductEntry)
+    }
 }
