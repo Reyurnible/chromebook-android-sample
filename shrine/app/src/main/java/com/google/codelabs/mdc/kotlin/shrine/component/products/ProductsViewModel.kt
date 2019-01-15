92d84b0ea77be50b031198a62b039d6fb8a320cf
ShunHosaka <reyurnible@gmail.com>
2019-01-16 01:12:59 +0900

    [add] rxscheduler request

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/component/products/ProductsViewModel.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/component/products/ProductsViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 
 class ProductsViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: ProductRepository = ProductRepository(application)
@@ -22,9 +25,28 @@ class ProductsViewModel(application: Application) : AndroidViewModel(application
             value = false
         }
     }
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     init {
-        productList.value = repository.fetchList()
+        repository.fetchList()
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                isLoading.value = true
+            }
+            .doOnEvent { _, _ ->
+                isLoading.value = false
+            }
+            .subscribe({
+                productList.value = it
+            }, {
+
+            })
+            .addTo(compositeDisposable)
+    }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
     }
 
     fun onClickAddCart(product: ProductEntry) {
--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductRepository.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductRepository.kt
@@ -3,20 +3,27 @@ package com.google.codelabs.mdc.kotlin.shrine.network
 import android.content.Context
 import com.google.codelabs.mdc.kotlin.shrine.R
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import java.io.BufferedReader
 import java.util.ArrayList
+import java.util.concurrent.TimeUnit
 
 class ProductRepository(
     context: Context
 ) {
     private val resources = context.resources
+    private val gson: Gson = GsonBuilder().create()
 
-    fun fetchList(): List<ProductEntry> {
-        val inputStream = resources.openRawResource(R.raw.products)
-        val jsonProductsString = inputStream.bufferedReader().use(BufferedReader::readText)
-        val gson = Gson()
-        val productListType = object : TypeToken<ArrayList<ProductEntry>>() {}.type
-        return gson.fromJson<List<ProductEntry>>(jsonProductsString, productListType)
-    }
+    fun fetchList(): Single<List<ProductEntry>> =
+        Single.timer(3L, TimeUnit.SECONDS)
+            .flatMap {
+                val inputStream = resources.openRawResource(R.raw.products)
+                val jsonProductsString = inputStream.bufferedReader().use(BufferedReader::readText)
+                val productListType = object : TypeToken<ArrayList<ProductEntry>>() {}.type
+                Single.just(gson.fromJson<List<ProductEntry>>(jsonProductsString, productListType))
+            }
+            .subscribeOn(Schedulers.io())
 }
